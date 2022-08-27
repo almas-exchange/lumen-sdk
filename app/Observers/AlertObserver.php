350aebdae4e062f8de3d4eb8a603daf336673998
Vahid Ashourzadeh <v.ashourzadeh@gmail.com>
2022-08-27 15:41:58 +0430

    Update AlertObserver.php

--- app/Observers/AlertObserver.php
+++ app/Observers/AlertObserver.php
@@ -10,7 +10,7 @@ class AlertObserver
     {
         try {
             \Bschmitt\Amqp\Facades\Amqp::publish('',
-                json_encode(['event' => 'alert', 'user' => $alert->user_id, 'data' => $alert->toArray()]),
+                json_encode(['event' => 'alert', 'user' => $alert->user_id, 'data' => $alert->makeHidden(['user_id', 'updated_at'])->toArray()]),
                 ['exchange' => 'exchange.websocket', 'exchange_type' => 'fanout']
             );
         } catch (\Throwable $throwable) {
